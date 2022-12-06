8ef7d2f3126c26ebf9ddb5e0664c89fe700557ea
Nut01 <99573711+Nut01@users.noreply.github.com>
2022-12-07 02:24:52 +0700

    update Port to 4000

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const app = express();
 
 global.__basedir = __dirname;
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "http://localhost:4000",
   credentials: true,
 };
 
